5e61b3b4f620eac2b176732c83e4b7052d8205d6
hbao448 <hbao448@aucklanduni.ac.nz>
2017-10-06 23:05:51 +1300

    Created new cutscene branch without library, build, obj and without Demo parent folder

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour {
+
+    public Text dText;
+    public Text nextText;
+    public List<string> dLines;
+    private int count;
+    public LoadOnScreenClick loader;
+    public LoadOrSave saveLoader;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        updateText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (count == dLines.Count && Input.anyKeyDown)
+        {
+            saveLoader.Load();
+            loader.LoadByIndex();
+        }else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(count);
+            updateText();
+        }
+	}
+
+    private void updateText()
+    {
+        if (count < dLines.Count)
+        {
+            dText.text = dLines[count];
+            count++;
+            if (count == dLines.Count)
+            {
+                nextText.text = "Press Any Key to Return...";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public GameObject quitPanel;
+    public AudioSource pingu;
+
+    private bool quitMenuOpen;
+    private Animator quitter;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetCountText(SaveStateController.controller.health);
+        winText.text = "";
+        quitMenuOpen = false;
+        quitter = quitPanel.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!quitMenuOpen)
+            {
+                quitMenuOpen = true;
+                quitter.SetTrigger("Open");
+            }
+            else {
+                quitter.SetTrigger("Close");
+                quitMenuOpen = false;
+            }
+        }
+    }
+
+    public void closeQuitMenu()
+    {
+        quitter.SetTrigger("Close");
+        quitMenuOpen = false;
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            //count = count + 1;
+            Debug.Log(SaveStateController.controller.health);
+            SaveStateController.controller.health += 1;
+            SetCountText(SaveStateController.controller.health);
+            pingu.Play();
+        }
+    }
+
+    void SetCountText(int count)
+    {
+
+        countText.text = "Health: " + count.ToString();
+        if (count >= 12)
+        {
+            //winText.text = "\\(You . Win)/";
+        }
+    }
+}

bbe0a34a032810b4ef053695717acf407bed4883
Andy Tang <andytang247@gmail.com>
2017-10-06 18:10:39 +1300

    Added laser controlled door

--- Assets/LaserAndMirror/Scripts/LaserReceiverController.cs
+++ Assets/LaserAndMirror/Scripts/LaserReceiverController.cs
@@ -3,9 +3,55 @@ using System.Collections;
 
 public class LaserReceiverController : MonoBehaviour {
 
-    // Inherit this class and override this method to add behaviour when laser hits receiver.
+    public GameObject door;
+    public float speed;
+
+    private float doorOpenPosition;
+    private float doorClosePosition;
+    private bool isClosing;
+
+    private void Start()
+    {
+        isClosing = true;
+
+        doorClosePosition = door.transform.localPosition.y;
+        doorOpenPosition = doorClosePosition + 2.8f;
+    }
+
+    private void Update()
+    {
+        float delta = Time.deltaTime;
+        Vector3 doorPos = door.transform.localPosition;
+
+        if (isClosing)
+        {
+            float newY = door.transform.localPosition.y - (speed * delta);
+
+            if (newY < doorClosePosition)
+            {
+                newY = doorClosePosition;
+            }
+
+            door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
+        }
+
+        else
+        {
+            float newY = door.transform.localPosition.y + (speed * delta);
+
+            if (newY > doorOpenPosition)
+            {
+                newY = doorOpenPosition;
+            }
+
+            door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
+            isClosing = true;
+        }
+    }
+
     public void activate()
     {
+        isClosing = false;
         print("Receiving Laser!!");
     }
 

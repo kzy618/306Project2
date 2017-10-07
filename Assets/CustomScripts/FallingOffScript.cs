68d04f396f575bc1abef1734700f72fd476b803b
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-08 12:07:44 +1300

    added folling objects mechanic

---
+++ Assets/CustomScripts/FallingOffScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingOffScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

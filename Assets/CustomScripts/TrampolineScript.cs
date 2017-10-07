fd7c10953a1de5424c98e49d34ed3776ad1e8cd0
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-08 01:59:30 +1300

    added trampoline
    Bug: doesnt work on the new FPScontroller

---
+++ Assets/CustomScripts/TrampolineScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("TAMPOLINE on "+col.gameObject.tag);
+		Rigidbody rb = col.gameObject.GetComponent<Rigidbody> ();
+		if (rb != null) {
+			Debug.Log ("Jump");
+			rb.velocity = new Vector3 (0f, 5f, 0f);
+		} else {
+			if(col.gameObject.CompareTag("Player")){
+				Debug.Log ("RB is null");
+			}
+		}
+	}
+}

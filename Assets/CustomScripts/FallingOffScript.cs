e5b993ac07f6daf77f757aaa0e7d42f31ae4413f
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-08 12:44:18 +1300

    fallingoffScript

--- Assets/CustomScripts/FallingOffScript.cs
+++ Assets/CustomScripts/FallingOffScript.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class FallingOffScript : MonoBehaviour {
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = gameObject.GetComponent<Rigidbody> ();
+		rb.constraints = RigidbodyConstraints.FreezePosition;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	void OnTriggerEnter(Collider col){
+		rb.constraints = RigidbodyConstraints.None;
 	}
 }

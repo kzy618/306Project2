f6f03cf3a0d35c21f26d3e73ab55f941c16ef7db
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2017-10-07 16:57:19 +1300

    Made non blank name mandatory

--- Assets/UI/Scripts/Menu/NamePanelController.cs
+++ Assets/UI/Scripts/Menu/NamePanelController.cs
@@ -4,10 +4,30 @@ using UnityEngine.UI;
 
 public class NamePanelController : MonoBehaviour {
     public InputField nameField;
-	
+    public Button confirmButton;
+
+    void Start()
+    {
+        // change the input field to "" to disable confirm button
+        nameField.text = "Hi";
+        nameField.text = "";
+    }
+
     // stores the new name
-	public void SetNewName () {
+    public void SetNewName () {
         SaveStateController.controller.playerName = nameField.text;
+    }
+
+    // disables the confirm button if the username field is blank
+    public void ReconfigureConfirmButton() {
+        if (nameField.text.Trim() == "")
+        {
+            confirmButton.interactable = false;
+        }
+        else {
+            confirmButton.interactable = true;
+
+        }
     }
 
     // focuses the name field

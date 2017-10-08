f50ac9d49f3ea931f3017100de6022c3a441d0e8
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-09 02:02:49 +1300

    tried implementing conveyer belt but the architecture might need changing

---
+++ Assets/Levels/CustomScripts/MovingPlatform/ConveyerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyerScript : MonoBehaviour {
+	/*
+	public GameObject start;
+	public GameObject end;
+	public float conveyerSpeed = 1f;
+
+	private Vector3 direction;
+	private GameObject currentConveyerUnit; //conveyer unit that is currently on this gameobject
+
+	// Use this for initialization
+	void Start () {
+		direction = (end.transform.position - start.transform.position).normalized;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+		
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.CompareTag("conveyerBeltUnit")){
+			currentConveyerUnit = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.CompareTag("conveyerBeltUnit")){
+			currentConveyerUnit = null;
+		}
+	}*/
+}
---
+++ Assets/Levels/CustomScripts/MovingPlatform/ConveyerStartScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyerStartScript : MonoBehaviour {
+
+	public GameObject end;
+	public float conveyerSpeed = 1f;
+
+	private Vector3 direction;
+	private GameObject currentConveyerUnit; //conveyer unit that is currently on this gameobject
+
+	// Use this for initialization
+	void Start () {
+		direction = (end.transform.position - transform.position).normalized;
+		GameObject startPlatform = Instantiate (Resources.Load ("ConveyerPlatform")) as GameObject;
+		startPlatform.transform.position = transform.position;
+		startPlatform.transform.rotation = transform.rotation;
+		Rigidbody startRb = startPlatform.GetComponent<Rigidbody> ();
+		startRb.velocity = direction * conveyerSpeed;
+		Debug.Log ("hi");
+		BoxCollider startBc  = startPlatform.GetComponent<BoxCollider> ();
+
+		currentConveyerUnit = startPlatform;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.CompareTag("conveyerBeltUnit")){
+
+			Debug.Log ("Conveyer Enter!");
+			currentConveyerUnit = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.CompareTag("conveyerBeltUnit")){
+			Debug.Log ("Conveyer Exit!");
+			currentConveyerUnit = null;
+
+
+			GameObject startPlatform = Instantiate (Resources.Load ("ConveyerPlatform")) as GameObject;
+			startPlatform.transform.position = transform.position;
+			startPlatform.transform.rotation = transform.rotation;
+			Rigidbody startRb = startPlatform.GetComponent<Rigidbody> ();
+			startRb.velocity = direction * conveyerSpeed;
+			Debug.Log (startRb.velocity);
+		}
+	}
+}

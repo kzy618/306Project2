7ae5a1f057a09f45bf86c3d006cca2e927035ccf
Andy Tang <andytang247@gmail.com>
2017-10-10 18:01:16 +1300

    Refactor LaserReceiverController to adhere to coding guidelines

--- Assets/Levels/CustomPackage/LaserAndMirror/Scripts/LaserReceiverController.cs
+++ Assets/Levels/CustomPackage/LaserAndMirror/Scripts/LaserReceiverController.cs
@@ -3,56 +3,59 @@ using System.Collections;
 
 public class LaserReceiverController : MonoBehaviour {
 
-    public GameObject door;
-    public float speed;
+    public GameObject _door;
+    public float _speed;
 
-    private float doorOpenPosition;
-    private float doorClosePosition;
-    private bool isClosing;
+    private float _doorOpenPosition;
+    private float _doorClosePosition;
+    private bool _isClosing;
 
     private void Start()
     {
-        isClosing = true;
+        _isClosing = true;
 
-        doorClosePosition = door.transform.localPosition.y;
-        doorOpenPosition = doorClosePosition + 2.8f;
+        _doorClosePosition = _door.transform.localPosition.y;
+        _doorOpenPosition = _doorClosePosition + 2.8f;
     }
 
     private void Update()
     {
         float delta = Time.deltaTime;
-        Vector3 doorPos = door.transform.localPosition;
+        Vector3 doorPos = _door.transform.localPosition;
 
-        if (isClosing)
+        // move door down
+        if (_isClosing)
         {
-            float newY = door.transform.localPosition.y - (speed * delta);
+            float newY = _door.transform.localPosition.y - (_speed * delta);
 
-            if (newY < doorClosePosition)
+            // ensures that door doesn't go below a certain position
+            if (newY < _doorClosePosition)
             {
-                newY = doorClosePosition;
+                newY = _doorClosePosition;
             }
 
-            door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
+            _door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
         }
 
+        // move door up
         else
         {
-            float newY = door.transform.localPosition.y + (speed * delta);
+            float newY = _door.transform.localPosition.y + (_speed * delta);
 
-            if (newY > doorOpenPosition)
+            // ensures that door doesn't go above a certain position
+            if (newY > _doorOpenPosition)
             {
-                newY = doorOpenPosition;
+                newY = _doorOpenPosition;
             }
 
-            door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
-            isClosing = true;
+            _door.transform.localPosition = new Vector3(doorPos.x, newY, doorPos.z);
+            _isClosing = true; // set to true in case the laser is no longer being received
         }
     }
 
     public void activate()
     {
-        isClosing = false;
-        //print("Receiving Laser!!");
+        _isClosing = false;
     }
 
 }

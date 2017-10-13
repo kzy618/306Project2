19434c8cdd76d27e3e7afc3c7fad08986a1c1dc2
hbao448 <hbao448@aucklanduni.ac.nz>
2017-10-14 02:15:17 +1300

    tagged ground in new level, prevented player from getting stuck in room

--- Assets/Levels/Scripts/ShedFloor.cs
+++ Assets/Levels/Scripts/ShedFloor.cs
@@ -6,14 +6,14 @@ public class ShedFloor : MonoBehaviour {
 	public GameObject _btn;
 
 	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag ("Player")) {
+		if (other.CompareTag ("Player") || other.CompareTag ("pickable")) {
 			Debug.Log ("ENTER FLOOR");
 			_btn.GetComponent<DoorButton> ().enabled = false;
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
-		if (other.CompareTag ("Player")) {
+		if (other.CompareTag ("Player") || other.CompareTag ("pickable")) {
 			Debug.Log ("EXIT FLOOR");
 			_btn.GetComponent<DoorButton> ().enabled = true;
 		}

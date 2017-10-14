96431800824e4f9b48129604a67b20b6bf3542b0
hbao448 <hbao448@aucklanduni.ac.nz>
2017-10-14 16:41:24 +1300

    tagged ground again, added measures to prevent player getting stuck in gatehouse

--- Assets/Levels/Scripts/ShedFloor.cs
+++ Assets/Levels/Scripts/ShedFloor.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class ShedFloor : MonoBehaviour {
 
 	public GameObject _btn;
+    private Collider _other;
 
-	void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Player") || other.CompareTag ("pickable")) {
 			Debug.Log ("ENTER FLOOR");
-			_btn.GetComponent<DoorButton> ().enabled = false;
+			_btn.GetComponent<DoorButton>().enabled = false;
+            _other = other;
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
 		if (other.CompareTag ("Player") || other.CompareTag ("pickable")) {
 			Debug.Log ("EXIT FLOOR");
-			_btn.GetComponent<DoorButton> ().enabled = true;
+			_btn.GetComponent<DoorButton>().enabled = true;
 		}
 	}
+
+    void FixedUpdate() {
+        if (!_other)
+        {
+            _btn.GetComponent<DoorButton>().enabled = true;
+        }
+    }
 }

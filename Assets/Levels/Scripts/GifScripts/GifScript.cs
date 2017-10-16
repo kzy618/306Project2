8f08b18dc9031b80c1730a1c639f060e357988fc
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-16 16:32:48 +1300

    added a sercet chaika in the level

--- Assets/Levels/Scripts/GifScripts/GifScript.cs
+++ Assets/Levels/Scripts/GifScripts/GifScript.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class GifScript : MonoBehaviour {
 
 	public Texture[] textures;
+	public int[] delayedFrames; //which frames to delay
+	public int[] delayLength;	//how long the frames selected are delayed
 
 	private int i;
 	private int j;
 	public int toNextFrame = 5;
 	private int size;
+	private	int delay = 0;
 	// Use this for initialization
 	void Start () {
 		size = textures.Length;
@@ -22,7 +25,13 @@ public class GifScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		j++;
-		if (j >= toNextFrame) {
+		int delayIdx = isInDelayFrames (i);
+		if (delayIdx != -1) {
+			delay = delayLength [delayIdx];
+		} else {
+			delay = 0;
+		}
+		if (j >= toNextFrame + delay) {
 			j = 0;
 			i++;
 			if (i >= size) {
@@ -30,8 +39,17 @@ public class GifScript : MonoBehaviour {
 			}
 		}
 
+
 		Renderer rend = GetComponent<Renderer> ();
 		rend.material.mainTexture = textures [i];
+	}
+
+	int isInDelayFrames(int frame){
+		for (int i=0; i<delayedFrames.Length; i++) {
+			if (frame == delayedFrames[i])
+				return i;
+		}
+		return -1;
 	}
 
 }

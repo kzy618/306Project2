2cfc7ad3414efd2d8a48d4b8d8b7f7755866eb4d
DarkIris3196 <jc.dumogho@gmail.com>
2017-10-22 14:21:06 +1300

    implemented hospital room

--- Assets/Levels/Scripts/DroppingWallScript.cs
+++ Assets/Levels/Scripts/DroppingWallScript.cs
@@ -2,14 +2,49 @@
 using System.Collections;
 
 public class DroppingWallScript : MonoBehaviour {
-
+	public float timerSec = 0f;
+	public GameObject front;
+	public GameObject back;
+	public GameObject left;
+	public GameObject right;
+	public GameObject roof;
+	public GameObject red;
 	// Use this for initialization
+
+	private float speed;
+	private float generic;
+	private bool activated = false;
 	void Start () {
-	
+		speed = .1f;
+		generic = 90f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!activated) {
+			
+		}else if(timerSec > 0f){
+
+			timerSec -= Time.time;
+		} else if (generic >= 0) {
+			front.transform.Rotate (new Vector3 (-speed, 0f, 0f));
+			back.transform.Rotate (new Vector3 (speed, 0f, 0f));
+			left.transform.Rotate (new Vector3 (0f, 0f, speed));
+			right.transform.Rotate (new Vector3 (0f, 0f, -speed));
+			roof.transform.localScale = roof.transform.localScale/speed;
+			speed *= 1.05f;
+			generic -= speed;
+		} else {
+			Destroy (front);
+			Destroy (back);
+			Destroy (left);
+			Destroy (right);
+			Destroy (roof);
+			red.SetActive (true);
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		activated = true;
 	}
 }

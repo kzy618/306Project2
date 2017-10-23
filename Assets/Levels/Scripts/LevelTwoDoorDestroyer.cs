5d68751ff14beaa88ada558aa47bb9789a7f18e3
kzy618 <zkon584@aucklanduni.ac.nz>
2017-10-23 23:55:38 +1300

    lvl 2

---
+++ Assets/Levels/Scripts/LevelTwoDoorDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwoDoorDestroyer : MonoBehaviour {
+
+	public GameObject Door;
+
+	private bool done;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void activate()
+	{
+		if (!done)
+		{
+			Destroy (Door);
+			done = true;
+		}
+	}
+}
